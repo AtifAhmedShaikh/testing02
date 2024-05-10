d403ba9569ee9e02c3bc0a95c1af91880cb0b822
AtifAhmadShaikh <atifahmad2219@gmail.com>
2024-05-10 11:44:12 +0500

    Fix files and folders path

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UVJ/QkMLkMkM8gcbh0jeSC/lTVo702BlNngg2XxPKU0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
-import { getAllTodos } from "./Components/apis";
-import AddTask from "./Components/AddTask";
-import TodoList from "./Components/TodoList";
+import { getAllTodos } from "../services/TodoService";
+import AddTask from "../components/AddTask";
+import TodoList from "../components/TodoList";
 
 //const dataFilePath = path.join(process.cwd(), 'json/userData.json');
 
 export default async function Home() {
   const tasks = await getAllTodos();
 
- // console.log(tasks);
-  
- return (
+  // console.log(tasks);
+
+  return (
     <main className="max-w-4xl mx-auto mt-4">
-      <div className="text-center my-5 flex flex-col gap-4">
+      <div className="flex flex-col gap-4 my-5 text-center">
         <h1 className="text-2xl font-bold">Tasks List</h1>
         <AddTask />
       </div>
       <div className="text-center">
-        <TodoList tasks={tasks} />
+        {tasks?.length && <TodoList tasks={tasks} />}
       </div>
     </main>
   );
